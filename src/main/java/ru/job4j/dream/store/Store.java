2f56bb4830b4a08701b9cb01c39b14c0428cdc4e
Semen Slepakov <educationkvv@mail.ru>
2021-03-16 14:22:40 +0300

    4. candicates.jsp - список кандидатов.[276522#482232]

--- src/main/java/ru/job4j/dream/store/Store.java
+++ src/main/java/ru/job4j/dream/store/Store.java
@@ -13,7 +13,7 @@ public class Store {
 
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
 
-    private Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
+    private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
     private Store() {
         posts.put(1, new Post(1, "Junior Java Job"));
