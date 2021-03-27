9c4901d751b0ea38880bb1e2e9b0112c19963548
Semen Slepakov <educationkvv@mail.ru>
2021-03-27 18:50:32 +0300

    1. Написать тест на PostServlet.[2518#482254]

--- src/test/java/ru/job4j/dream/servlet/PostServletTest.java
+++ src/test/java/ru/job4j/dream/servlet/PostServletTest.java
@@ -10,6 +10,8 @@ import ru.job4j.dream.store.MemStore;
 import ru.job4j.dream.store.PsqlStore;
 import ru.job4j.dream.store.Store;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -32,5 +34,18 @@ public class PostServletTest {
         Mockito.when(req.getParameter("name")).thenReturn("Petr Arsentev");
         new PostServlet().doPost(req, resp);
         assertThat(store.findAllPosts().iterator().next().getName(), is("Petr Arsentev"));
+    }
+
+    @Test
+    public void whenDoGotThenCheckDispatcher() throws ServletException, IOException {
+        Store store = MemStore.instOf();
+        PowerMockito.mockStatic(PsqlStore.class);
+        PowerMockito.when(PsqlStore.instOf()).thenReturn(store);
+        HttpServletRequest req = Mockito.mock(HttpServletRequest.class);
+        HttpServletResponse resp = Mockito.mock(HttpServletResponse.class);
+        RequestDispatcher dispatcher = Mockito.mock(RequestDispatcher.class);
+        Mockito.when(req.getRequestDispatcher("posts.jsp")).thenReturn(dispatcher);
+        new PostServlet().doGet(req, resp);
+        Mockito.verify(req).getRequestDispatcher("posts.jsp");
     }
 }
